aecdbb7404728539142adafcef87f94513da5f0e
Artcro <arthur.croce@al.infnet.edu.br>
2024-10-23 10:01:18 -0300

    login and register js

--- js/app-main.js
+++ js/app-main.js
@@ -4,26 +4,36 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 class Usuario {
-	constructor(nome, email) {
+	constructor(nome, senha, email) {
 		this.nome = nome;
+		this.senha = senha;
 		this.email = email;
-		this.nascimento = new Date();
-	}
-	idade() {
-		// birthday is a date
-		var ageDifMs = Date.now() - this.nascimento;
-		var ageDate = new Date(ageDifMs); // miliseconds from 1970
-		return Math.abs(ageDate.getUTCFullYear() - 1970);
 	}
 }
 
 class Usuarios {
 	constructor() {
 		this.usuarios = [];
+		this.load();
 	}
-	novoUsuario(nome, email) {
-		let usuario = new Usuario(nome, email);
+	load() {
+		let users = alasql(`SELECT * FROM users`);
+		users.forEach((user) => {
+			let usuario = new Usuario(user[0], user[1], user[2]);
+		});
+		console.log(this.usuarios);
+	}
+	novoUsuario(nome, senha, email) {
+		let usuario = new Usuario(nome, senha, email);
+		if (this.isCadastrado(usuario)) {
+			return undefined;
+		}
+
 		this.usuarios.push(usuario);
+		alasql(
+			`INSERT INTO users (name, password, email) VALUES ('${nome}', '${senha}', '${email}')`
+		);
+		console.log(alasql("SELECT * FROM users"));
 		return this.isCadastrado(usuario) ? undefined : usuario;
 	}
 	removerUsuario(nome) {
@@ -38,7 +48,17 @@ class Usuarios {
 	getUsuario(nome) {
 		return this.usuarios.find((user) => user.nome == nome);
 	}
-	isCadastrado(nome) {
+	checkPassword(nome, senha) {
+		return this.getUsuario(nome).senha == senha;
+	}
+	isCadastrado(objUsuario) {
+		return this.usuarios.includes(objUsuario);
+		// return (
+		// 	this.usuarios.find((user) => user.nome == objUsuario.nome) !=
+		// 	undefined
+		// );
+	}
+	isCadastradoNome(nome) {
 		return this.usuarios.find((user) => user.nome == nome) != undefined;
 	}
 	isCadastradoEmail(email) {
@@ -54,65 +74,63 @@ class Produto {
 	}
 }
 
-let usuariosCadastrados = new Usuarios();
-
-function cadastrarSolicitante() {
-	let nome = document.getElementById("nome").value;
-	let email = document.getElementById("email").value;
-	let listaSolicitantes = document.getElementById("solicitantes");
-
-	if (usuariosCadastrados.isCadastrado(nome)) {
-		let li = document.createElement("li");
-		li.textContent = "Usuario ja cadastrado.";
-		listaSolicitantes.appendChild(li);
-		return;
-	}
-
-	if (usuariosCadastrados.isCadastradoEmail(email)) {
-		let li = document.createElement("li");
-		li.textContent = "Email ja utilizado por outro usuário!";
-		listaSolicitantes.appendChild(li);
-		return;
-	}
-
-	usuariosCadastrados.novoUsuario(nome, email);
-
-	const li = document.createElement("li");
-	li.textContent = usuariosCadastrados.isCadastrado(nome)
-		? "Usuario Cadastrado! Nome: " +
-		  usuariosCadastrados.getUsuario(nome).nome
-		: "Erro no cadastro.";
-
-	listaSolicitantes.appendChild(li);
-}
-
 // localStorage.clear();
 
 alasql("CREATE localStorage DATABASE IF NOT EXISTS projeto_db");
 alasql("ATTACH localStorage DATABASE projeto_db");
 alasql("USE projeto_db");
-// alasql("DROP TABLE IF EXISTS disciplinas");
+alasql("DROP TABLE IF EXISTS users");
+
 alasql(
-	"CREATE TABLE IF NOT EXISTS disciplinas (id int AUTO_INCREMENT, category_id int, name string, created_at DATE)"
+	"CREATE TABLE IF NOT EXISTS users (name string, password VARCHAR(255), email string)"
 );
-
-let data = Date.now().toString();
 alasql(
-	`INSERT INTO disciplinas (id, category_id, name, created_at) VALUES (1, 1, 'car1', '${data}')`
+	`INSERT INTO users (name, password, email) VALUES ('Arthur', 'Croce', '@')`
 );
+// console.log(localStorage.getItem("projeto_db.disciplinas"));
 
-// alasql('CREATE localStorage DATABASE IF NOT EXISTS Atlas');
-// alasql('ATTACH localStorage DATABASE Atlas AS MyAtlas');
-// alasql('CREATE TABLE IF NOT EXISTS MyAtlas.City (city string, population number)');
-// alasql('SELECT * INTO MyAtlas.City FROM ?',[ [
-//         {city:'Vienna', population:1731000},
-//         {city:'Budapest', population:1728000}
-// ] ]);
-// var res = alasql('SELECT * FROM MyAtlas.City');
+let usuariosCadastrados = new Usuarios();
 
-console.log(alasql("SELECT id FROM disciplinas"));
+function registerUser() {
+	let nome = document.getElementById("input-username").value;
+	let senha = document.getElementById("input-password").value;
+	let email = "@";
 
-console.log(localStorage.getItem("projeto_db.disciplinas"));
+	let returnDiv = document.getElementById("login-message");
+
+	if (usuariosCadastrados.isCadastradoNome(nome)) {
+		returnDiv.textContent = "Usuario ja cadastrado.";
+		return;
+	}
+
+	// if (usuariosCadastrados.isCadastradoEmail(email)) {
+	// 	returnDiv.textContent = "Email ja utilizado por outro usuário!";
+	// 	return;
+	// }
+
+	usuariosCadastrados.novoUsuario(nome, senha, email);
+}
+
+function login() {
+	let nome = document.getElementById("input-username").value;
+	let senha = document.getElementById("input-password").value;
+
+	let returnDiv = document.getElementById("login-message");
+
+	if (!usuariosCadastrados.isCadastradoNome(nome)) {
+		returnDiv.textContent =
+			"Usuário não encontrado. Favor realizar o cadastro primeiro.";
+		return;
+	}
+
+	if (!usuariosCadastrados.checkPassword(nome, senha)) {
+		returnDiv.textContent = "Senha incorreta favor tentar novamente.";
+		return;
+	}
+
+	sessionStorage.setItem("loginSession", nome);
+	returnDiv.textContent = "Login realizado com Sucesso.";
+}
 
 function insertToDatabase() {}
 function removeFromDatabase() {}
