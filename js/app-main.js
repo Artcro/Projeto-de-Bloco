690a71ec41d58df1bcde704798560e204927e0f0
Artcro <arthur.croce.croce@gmail.com>
2024-10-29 14:20:40 -0300

    Intelij Configs

--- js/app-main.js
+++ js/app-main.js
@@ -1,5 +1,6 @@
 canLoadProtectedPage();
-window.addEventListener("load", () => {
+window.addEventListener("load", () =>
+{
 	showLoggedNavBar();
 });
 
@@ -11,18 +12,24 @@ alasql("ATTACH localStorage DATABASE projeto_db");
 alasql("USE projeto_db");
 alasql("DROP TABLE IF EXISTS users");
 
-alasql(
-	"CREATE TABLE IF NOT EXISTS users (name string, password VARCHAR(255), email string, is_admin BOOLEAN, is_teacher BOOLEAN)"
-);
-alasql(`INSERT INTO users VALUES ('Arthur', 'Croce', '@', true, true)`);
-alasql(`INSERT INTO users VALUES ('Maria', 'Santos', '@', false, true)`);
-alasql(`INSERT INTO users VALUES ('Pedro', 'Silva', '@', false, false)`);
-alasql(`INSERT INTO users VALUES ('João', 'Almeida', '@', false, false)`);
-alasql(`INSERT INTO users VALUES ('Luiza', 'Ribeiro', '@', false, false)`);
+alasql("CREATE TABLE IF NOT EXISTS users (name string, password VARCHAR(255), email string, is_admin BOOLEAN, is_teacher BOOLEAN)");
+alasql(`INSERT INTO users
+        VALUES ('Arthur', 'Croce', '@', true, true)`);
+alasql(`INSERT INTO users
+        VALUES ('Maria', 'Santos', '@', false, true)`);
+alasql(`INSERT INTO users
+        VALUES ('Pedro', 'Silva', '@', false, false)`);
+alasql(`INSERT INTO users
+        VALUES ('João', 'Almeida', '@', false, false)`);
+alasql(`INSERT INTO users
+        VALUES ('Luiza', 'Ribeiro', '@', false, false)`);
+
 // console.log(localStorage.getItem("projeto_db.disciplinas"));
 
-class User {
-	constructor(name, password, email, is_admin, is_teacher) {
+class User
+{
+	constructor(name, password, email, is_admin, is_teacher)
+	{
 		this.name = name;
 		this.password = password;
 		this.email = email;
@@ -31,85 +38,118 @@ class User {
 	}
 }
 
-class Student extends User {
-	constructor(name, password, email) {
+class Student extends User
+{
+	constructor(name, password, email)
+	{
 		super(name, password, email, false, true);
 	}
 }
 
-class Teacher extends User {
-	constructor(name, password, email) {
+class Teacher extends User
+{
+	constructor(name, password, email)
+	{
 		super(name, password, email, false, true);
 	}
 }
 
-class UserPool {
-	constructor() {
+class UserPool
+{
+	constructor()
+	{
 		this.usuarios = this.load();
 	}
-	load() {
-		return alasql(`SELECT * FROM users`);
+
+	load()
+	{
+		return alasql(`SELECT *
+                       FROM users`);
 	}
-	novoUsuario(nome, senha, email) {
+
+	novoUsuario(nome, senha, email)
+	{
 		let usuario = new User(nome, senha, email);
-		if (this.isCadastrado(usuario)) {
+		if (this.isCadastrado(usuario))
+		{
 			return undefined;
 		}
 
 		this.usuarios.push(usuario);
-		alasql(
-			`INSERT INTO users VALUES ('${nome}', '${senha}', '${email}', 'false', 'false')`
-		);
+		alasql(`INSERT INTO users
+                VALUES ('${nome}', '${senha}', '${email}', 'false', 'false')`);
 		return this.isCadastrado(usuario) ? undefined : usuario;
 	}
-	removerUsuario(nome) {
+
+	removerUsuario(nome)
+	{
 		this.usuarios = this.usuarios.filter((user) => user != nome);
 	}
-	todosUsuarios() {
+
+	todosUsuarios()
+	{
 		return this.usuarios;
 	}
-	numeroDeUsuarios() {
+
+	numeroDeUsuarios()
+	{
 		return this.usuarios.length;
 	}
-	getUsuario(nome) {
+
+	getUsuario(nome)
+	{
 		return this.usuarios.find((user) => user.name == nome);
 	}
-	checkPassword(nome, senha) {
+
+	checkPassword(nome, senha)
+	{
 		return this.getUsuario(nome).password == senha;
 	}
-	isCadastrado(objUsuario) {
+
+	isCadastrado(objUsuario)
+	{
 		return this.usuarios.includes(objUsuario);
 		// return (
 		// 	this.usuarios.find((user) => user.name == objUsuario.name) !=
 		// 	undefined
 		// );
 	}
-	isCadastradoNome(nome) {
+
+	isCadastradoNome(nome)
+	{
 		return this.usuarios.find((user) => user.name == nome) != undefined;
 	}
-	isCadastradoEmail(email) {
+
+	isCadastradoEmail(email)
+	{
 		return this.usuarios.find((user) => user.email == email) != undefined;
 	}
 }
 
-class Disciplinas {
-	constructor(nome, tipo, detalhe) {
+class Disciplinas
+{
+	constructor(nome, tipo, detalhe)
+	{
 		this.name = nome;
 		this.tipo = tipo;
 		this.detalhe = detalhe;
 	}
 }
 
-class Curso {
-	constructor(nome, tipo, detalhe) {
+class Curso
+{
+	constructor(nome, tipo, detalhe)
+	{
 		this.name = nome;
 		this.tipo = tipo;
 		this.detalhe = detalhe;
 	}
 }
 
-class Turma {
-	constructor(nome, tipo, detalhe) {
+class Turma
+{
+	constructor(nome, tipo, detalhe)
+	{
 		this.name = nome;
 		this.tipo = tipo;
 		this.detalhe = detalhe;
@@ -119,14 +159,16 @@ class Turma {
 let usuariosCadastrados = new UserPool();
 console.log(usuariosCadastrados.todosUsuarios());
 
-function registerUser() {
+function registerUser()
+{
 	let nome = document.getElementById("input-username").value;
 	let senha = document.getElementById("input-password").value;
 	let email = "@";
 
 	let returnDiv = document.getElementById("login-message");
 
-	if (usuariosCadastrados.isCadastradoNome(nome)) {
+	if (usuariosCadastrados.isCadastradoNome(nome))
+	{
 		returnDiv.textContent = "Usuario ja cadastrado.";
 		return;
 	}
@@ -139,11 +181,13 @@ function registerUser() {
 	usuariosCadastrados.novoUsuario(nome, senha, email);
 }
 
-function isLoggedIn() {
+function isLoggedIn()
+{
 	return sessionStorage.getItem("loginSession") != null;
 }
 
-function login() {
+function login()
+{
 	let nome = document.getElementById("input-username").value;
 	let senha = document.getElementById("input-password").value;
 
@@ -152,13 +196,14 @@ function login() {
 	console.log(nome);
 	console.log(senha);
 
-	if (!usuariosCadastrados.isCadastradoNome(nome)) {
-		returnDiv.textContent =
-			"Usuário não encontrado. Favor realizar o cadastro primeiro.";
+	if (!usuariosCadastrados.isCadastradoNome(nome))
+	{
+		returnDiv.textContent = "Usuário não encontrado. Favor realizar o cadastro primeiro.";
 		return;
 	}
 
-	if (!usuariosCadastrados.checkPassword(nome, senha)) {
+	if (!usuariosCadastrados.checkPassword(nome, senha))
+	{
 		returnDiv.textContent = "Senha incorreta favor tentar novamente.";
 		console.log(usuariosCadastrados.todosUsuarios());
 		return;
@@ -170,30 +215,38 @@ function login() {
 	window.location = "./loggedOverview.html";
 }
 
-function logout() {
-	if (isLoggedIn()) {
+function logout()
+{
+	if (isLoggedIn())
+	{
 		sessionStorage.removeItem("loginSession");
-	} else {
+	}
+	else
+	{
 		alert("You are not logged into any account!");
 	}
 
 	window.location = "./index.html";
 }
 
-function canLoadProtectedPage() {
-	if (!isLoggedIn() && location.href.split("/").slice(-1) != "index.html") {
+function canLoadProtectedPage()
+{
+	if (!isLoggedIn() && location.href.split("/").slice(-1) != "index.html")
+	{
 		alert("You need to be logged in to access this page!");
 		window.location = "./index.html";
 		return;
 	}
 
-	if (isLoggedIn() && location.href.split("/").slice(-1) == "index.html") {
+	if (isLoggedIn() && location.href.split("/").slice(-1) == "index.html")
+	{
 		window.location = "./loggedOverview.html";
 		return;
 	}
 }
 
-function createNavLink(name, href, parentUl) {
+function createNavLink(name, href, parentUl)
+{
 	let navBarLi = document.createElement("li");
 	let navBarA = document.createElement("a");
 
@@ -205,7 +258,8 @@ function createNavLink(name, href, parentUl) {
 	parentUl.appendChild(navBarLi);
 }
 
-function showLoggedNavBar() {
+function showLoggedNavBar()
+{
 	let navBarLinks = [
 		["Home", "./index.html"],
 		["Disciplinas", "./disciplinas.html"],
@@ -213,14 +267,18 @@ function showLoggedNavBar() {
 		["Alunos", "./alunos.html"],
 	];
 
-	if (!isLoggedIn()) {
+	if (!isLoggedIn())
+	{
 		let navBarUl = document.querySelector(".nav-ul");
 		navBarUl.replaceChildren();
 		createNavLink(navBarLinks[0][0], navBarLinks[0][1], navBarUl);
-	} else {
+	}
+	else
+	{
 		let navBarUl = document.querySelector(".nav-ul");
 		navBarUl.replaceChildren();
-		navBarLinks.forEach((link) => {
+		navBarLinks.forEach((link) =>
+		{
 			createNavLink(link[0], link[1], navBarUl);
 		});
 	}
