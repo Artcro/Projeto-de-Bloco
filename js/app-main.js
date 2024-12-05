777b43390ae4968cf865360b17d15cdb0b25d0d7
Artcro <arthur.croce@al.infnet.edu.br>
2024-12-05 16:21:03 -0300

    Cadastro de Disciplinas

--- js/app-main.js
+++ js/app-main.js
@@ -4,184 +4,305 @@ window.addEventListener("load", () =>
 	//showLoggedNavBar();
 	const disciplinas = [
 		{
-			nome: "Matemática Básica", codigo: "MAT101", cargahoraria: 60, professor: "João Silva", curso: "Engenharia"
-		}, {
-			nome: "Programação em JavaScript",
-			codigo: "CS102",
-			cargahoraria: 80,
-			professor: "Maria Oliveira",
-			curso: "Ciência da Computação"
-		}, {
-			nome: "Introdução à Física", codigo: "FIS201", cargahoraria: 40, professor: "Carlos Mendes", curso: "Física"
-		}, {
-			nome: "Química Geral", codigo: "QUI301", cargahoraria: 50, professor: "Ana Pereira", curso: "Química"
-		}, {
-			nome: "Algoritmos e Estruturas de Dados",
-			codigo: "CS202",
-			cargahoraria: 70,
-			professor: "Paulo Castro",
-			curso: "Engenharia de Software"
-		}, {
-			nome: "História da Arte",
-			codigo: "ART101",
-			cargahoraria: 45,
-			professor: "Juliana Martins",
-			curso: "Arquitetura"
-		}, {
-			nome: "Gestão de Projetos",
-			codigo: "ADM401",
-			cargahoraria: 60,
-			professor: "Rodrigo Almeida",
-			curso: "Administração"
-		}, {
-			nome: "Economia Básica", codigo: "ECO101", cargahoraria: 40, professor: "Fernanda Costa", curso: "Economia"
-		}, {
-			nome: "Sistemas Operacionais",
-			codigo: "CS301",
-			cargahoraria: 75,
-			professor: "Ricardo Lopes",
-			curso: "Engenharia de Computação"
-		}, {
-			nome: "Marketing Digital",
-			codigo: "MKT201",
-			cargahoraria: 50,
-			professor: "Mariana Ribeiro",
-			curso: "Publicidade e Propaganda"
-		}, {
-			nome: "Cálculo Diferencial e Integral I",
-			codigo: "MAT201",
-			cargahoraria: 90,
-			professor: "Luís Gonzaga",
-			curso: "Engenharia"
-		}, {
-			nome: "Estruturas de Dados Avançadas",
-			codigo: "CS302",
-			cargahoraria: 70,
-			professor: "Cláudia Torres",
-			curso: "Ciência da Computação"
-		}, {
-			nome: "Mecânica Clássica", codigo: "FIS301", cargahoraria: 50, professor: "Sérgio Nunes", curso: "Física"
-		}, {
-			nome: "Bioquímica", codigo: "BIO101", cargahoraria: 60, professor: "Patrícia Almeida", curso: "Biologia"
-		}, {
-			nome: "Geometria Analítica",
-			codigo: "MAT202",
-			cargahoraria: 45,
-			professor: "André Barros",
-			curso: "Matemática"
-		}, {
-			nome: "Banco de Dados Relacionais",
-			codigo: "CS402",
-			cargahoraria: 80,
-			professor: "Renato Silva",
-			curso: "Sistemas de Informação"
-		}, {
-			nome: "Eletromagnetismo",
-			codigo: "FIS401",
-			cargahoraria: 70,
-			professor: "Elis Regina",
-			curso: "Engenharia Elétrica"
-		}, {
-			nome: "Química Orgânica", codigo: "QUI201", cargahoraria: 50, professor: "Leonardo Lima", curso: "Química"
-		}, {
-			nome: "Redes de Computadores",
-			codigo: "CS502",
-			cargahoraria: 60,
-			professor: "Gabriela Souza",
-			curso: "Engenharia de Computação"
-		}, {
-			nome: "Introdução à Filosofia",
-			codigo: "FIL101",
-			cargahoraria: 40,
-			professor: "Antônio Carlos",
-			curso: "Filosofia"
-		}, {
-			nome: "Gestão Financeira",
-			codigo: "ADM201",
-			cargahoraria: 55,
-			professor: "Fernanda Melo",
-			curso: "Administração"
-		}, {
-			nome: "Teoria da Computação",
-			codigo: "CS601",
-			cargahoraria: 75,
-			professor: "Rafael Dantas",
-			curso: "Ciência da Computação"
-		}, {
-			nome: "Psicologia Organizacional",
-			codigo: "PSI301",
-			cargahoraria: 50,
-			professor: "Débora Costa",
-			curso: "Psicologia"
-		}, {
-			nome: "Lógica Matemática",
-			codigo: "MAT301",
-			cargahoraria: 60,
-			professor: "Marcelo Andrade",
-			curso: "Engenharia de Software"
-		}, {
-			nome: "Direito Empresarial",
-			codigo: "DIR101",
-			cargahoraria: 40,
-			professor: "Paula Ribeiro",
-			curso: "Direito"
-		}, {
-			nome: "Segurança da Informação",
-			codigo: "CS701",
-			cargahoraria: 65,
-			professor: "Marcos Tavares",
-			curso: "Sistemas de Informação"
-		}, {
-			nome: "Microbiologia", codigo: "BIO201", cargahoraria: 70, professor: "Cristina Dias", curso: "Biologia"
-		}, {
-			nome: "Sociologia Aplicada",
-			codigo: "SOC101",
-			cargahoraria: 50,
-			professor: "Roberto Fernandes",
-			curso: "Sociologia"
-		}, {
-			nome: "Engenharia de Software",
-			codigo: "CS801",
-			cargahoraria: 80,
-			professor: "Luana Machado",
-			curso: "Engenharia de Computação"
-		}, {
-			nome: "Empreendedorismo",
-			codigo: "ADM301",
-			cargahoraria: 60,
-			professor: "Gustavo Oliveira",
-			curso: "Administração"
-		}
+		nome: "Matemática Básica", codigo: "MAT101", cargahoraria: 60, professor: "João Silva", curso: "Engenharia"
+	}, {
+		nome: "Programação em JavaScript",
+		codigo: "CS102",
+		cargahoraria: 80,
+		professor: "Maria Oliveira",
+		curso: "Ciência da Computação"
+	}, {
+		nome: "Introdução à Física", codigo: "FIS201", cargahoraria: 40, professor: "Carlos Mendes", curso: "Física"
+	}, {
+		nome: "Química Geral", codigo: "QUI301", cargahoraria: 50, professor: "Ana Pereira", curso: "Química"
+	}, {
+		nome: "Algoritmos e Estruturas de Dados",
+		codigo: "CS202",
+		cargahoraria: 70,
+		professor: "Paulo Castro",
+		curso: "Engenharia de Software"
+	}, {
+		nome: "História da Arte", codigo: "ART101", cargahoraria: 45, professor: "Juliana Martins", curso: "Arquitetura"
+	}, {
+		nome: "Gestão de Projetos",
+		codigo: "ADM401",
+		cargahoraria: 60,
+		professor: "Rodrigo Almeida",
+		curso: "Administração"
+	}, {
+		nome: "Economia Básica", codigo: "ECO101", cargahoraria: 40, professor: "Fernanda Costa", curso: "Economia"
+	}, {
+		nome: "Sistemas Operacionais",
+		codigo: "CS301",
+		cargahoraria: 75,
+		professor: "Ricardo Lopes",
+		curso: "Engenharia de Computação"
+	}, {
+		nome: "Marketing Digital",
+		codigo: "MKT201",
+		cargahoraria: 50,
+		professor: "Mariana Ribeiro",
+		curso: "Publicidade e Propaganda"
+	}, {
+		nome: "Cálculo Diferencial e Integral I",
+		codigo: "MAT201",
+		cargahoraria: 90,
+		professor: "Luís Gonzaga",
+		curso: "Engenharia"
+	}, {
+		nome: "Estruturas de Dados Avançadas",
+		codigo: "CS302",
+		cargahoraria: 70,
+		professor: "Cláudia Torres",
+		curso: "Ciência da Computação"
+	}, {
+		nome: "Mecânica Clássica", codigo: "FIS301", cargahoraria: 50, professor: "Sérgio Nunes", curso: "Física"
+	}, {
+		nome: "Bioquímica", codigo: "BIO101", cargahoraria: 60, professor: "Patrícia Almeida", curso: "Biologia"
+	}, {
+		nome: "Geometria Analítica", codigo: "MAT202", cargahoraria: 45, professor: "André Barros", curso: "Matemática"
+	}, {
+		nome: "Banco de Dados Relacionais",
+		codigo: "CS402",
+		cargahoraria: 80,
+		professor: "Renato Silva",
+		curso: "Sistemas de Informação"
+	}, {
+		nome: "Eletromagnetismo",
+		codigo: "FIS401",
+		cargahoraria: 70,
+		professor: "Elis Regina",
+		curso: "Engenharia Elétrica"
+	}, {
+		nome: "Química Orgânica", codigo: "QUI201", cargahoraria: 50, professor: "Leonardo Lima", curso: "Química"
+	}, {
+		nome: "Redes de Computadores",
+		codigo: "CS502",
+		cargahoraria: 60,
+		professor: "Gabriela Souza",
+		curso: "Engenharia de Computação"
+	}, {
+		nome: "Introdução à Filosofia",
+		codigo: "FIL101",
+		cargahoraria: 40,
+		professor: "Antônio Carlos",
+		curso: "Filosofia"
+	}, {
+		nome: "Gestão Financeira",
+		codigo: "ADM201",
+		cargahoraria: 55,
+		professor: "Fernanda Melo",
+		curso: "Administração"
+	}, {
+		nome: "Teoria da Computação",
+		codigo: "CS601",
+		cargahoraria: 75,
+		professor: "Rafael Dantas",
+		curso: "Ciência da Computação"
+	}, {
+		nome: "Psicologia Organizacional",
+		codigo: "PSI301",
+		cargahoraria: 50,
+		professor: "Débora Costa",
+		curso: "Psicologia"
+	}, {
+		nome: "Lógica Matemática",
+		codigo: "MAT301",
+		cargahoraria: 60,
+		professor: "Marcelo Andrade",
+		curso: "Engenharia de Software"
+	}, {
+		nome: "Direito Empresarial", codigo: "DIR101", cargahoraria: 40, professor: "Paula Ribeiro", curso: "Direito"
+	}, {
+		nome: "Segurança da Informação",
+		codigo: "CS701",
+		cargahoraria: 65,
+		professor: "Marcos Tavares",
+		curso: "Sistemas de Informação"
+	}, {
+		nome: "Microbiologia", codigo: "BIO201", cargahoraria: 70, professor: "Cristina Dias", curso: "Biologia"
+	}, {
+		nome: "Sociologia Aplicada",
+		codigo: "SOC101",
+		cargahoraria: 50,
+		professor: "Roberto Fernandes",
+		curso: "Sociologia"
+	}, {
+		nome: "Engenharia de Software",
+		codigo: "CS801",
+		cargahoraria: 80,
+		professor: "Luana Machado",
+		curso: "Engenharia de Computação"
+	}, {
+		nome: "Empreendedorismo",
+		codigo: "ADM301",
+		cargahoraria: 60,
+		professor: "Gustavo Oliveira",
+		curso: "Administração"
+	}
 
 	];
-	TableCreator.create(disciplinas);
+	//saveToLocalStorage("disciplinas", disciplinas);
+
+	if (document.getElementById("tableDisplay"))
+	{
+		TableCreator.create(MemoryStorage.disciplinas);
+	}
+
+
+	document.getElementById("cadastrar-disciplina").addEventListener("click", () => {
+		const form = document.getElementById("form-disciplinas");
+		let inputs = [...form.getElementsByTagName("input")];
+		inputs = inputs.map(input => input.value);
+
+		console.log(new Disciplinas(...inputs))
+		MemoryStorage.disciplinas = new Disciplinas(...inputs);
+	})
 });
 window.addEventListener("beforeunload", () =>
 {
-	saveToLocalStorage();
 })
+window.addEventListener("storage", (e) =>
+{
+
+	MemoryStorage.load(e);
+
+	// console.log(e.key);
+	// console.log(e.newValue);
+	// console.log(e.oldValue);
+	// console.log(e.storageArea);
+	// console.log(e.url);
+})
+
+class MemoryStorage
+{
+	static #usuarios = [];
+	static #disciplinas = [];
+	static #cursos = [];
+
+	static load(e)
+	{
+		switch (e.key)
+		{
+			case "users":
+				this.loadUsers();
+				break;
+			case "disciplinas":
+				this.loadDisciplinas();
+				break;
+			case "cursos":
+				this.loadCursos();
+				break;
+			default:
+				break;
+		}
+	}
+
+	static loadUsers()
+	{
+		this.#usuarios = loadFromLocalStorage("users");
+	}
+
+	static loadDisciplinas()
+	{
+		if (loadFromLocalStorage("disciplinas") && loadFromLocalStorage("disciplinas") > 0)
+		{
+			loadFromLocalStorage("disciplinas").forEach(disciplina =>
+			{
+				this.disciplinas = new Disciplinas(disciplina);
+			});
+		}
+	}
+
+	static loadCursos()
+	{
+		this.#cursos = loadFromLocalStorage("cursos");
+	}
+
+	static get usuarios()
+	{
+		if(this.#usuarios.length < 1)
+		{
+			this.loadUsers();
+		}
+		return this.#usuarios;
+	}
+
+	static get disciplinas()
+	{
+		if(this.#disciplinas.length < 1)
+		{
+			this.loadDisciplinas();
+		}
+		return this.#disciplinas;
+	}
+
+	static get cursos()
+	{
+		if(this.#cursos.length < 1)
+		{
+			this.loadCursos();
+		}
+		return this.#cursos;
+	}
+
+	static set usuarios(value)
+	{
+		this.#usuarios.push(value);
+		saveToLocalStorage("users", this.#usuarios);
+	}
+
+	static set disciplinas(value)
+	{
+		console.log(value);
+		this.#disciplinas.push(value);
+		console.log(this.#disciplinas);
+		saveToLocalStorage("disciplinas", this.#disciplinas);
+	}
+
+	static set cursos(value)
+	{
+		this.#cursos.push(value);
+		saveToLocalStorage("cursos", this.#cursos)
+	}
+
+}
 
 class User
 {
-	#name;
-	#password;
+	#id;
+	#username;
 	#email;
+	#password;
+	#access;
+	#name;
 
-	constructor(name, password, email)
+	constructor(username, password, access, name)
 	{
-		this.#name = name;
+		this.#username = username;
 		this.#password = password;
-		this.#email = email;
+		this.#access = access;
+		this.#name = name;
 	}
 
-	get name()
+	get id()
 	{
-		return this.#name;
+		return this.#id;
 	}
 
-	set name(value)
+	set id(value)
 	{
-		this.#name = value;
+		this.#id = value;
+	}
+
+	get username()
+	{
+		return this.#username;
+	}
+
+	set username(value)
+	{
+		this.#username = value;
 	}
 
 	get password()
@@ -204,84 +325,185 @@ class User
 		this.#email = value;
 	}
 
+	get access()
+	{
+		return this.#access;
+	}
+
+	set access(value)
+	{
+		this.#access = value;
+	}
+
+	save()
+	{
+		loadFromLocalStorage("users");
+		saveToLocalStorage()
+	}
+
 }
 
-class UserPool
+class Student extends User
 {
-	#users;
+	#matricula;
+	#curso;
+	#periodo;
+	#disciplinas = [];
 
-	constructor()
+	constructor(username, password, name, matricula)
 	{
-		this.#users = loadFromLocalStorage("users");
+		super(username, password, "student", name);
+		this.#matricula = matricula;
 	}
 
-	novoUsuario(nome, senha, email)
-	{
-		let usuario = new User(nome, senha, email);
-		if (this.isCadastrado(usuario))
-		{
-			return undefined;
-		}
 
-		this.usuarios.push(usuario);
-		return this.isCadastrado(usuario) ? undefined : usuario;
+	get matricula()
+	{
+		return this.#matricula;
 	}
 
-	removerUsuario(nome)
+	set matricula(value)
 	{
-		this.usuarios = this.usuarios.filter((user) => user != nome);
+		this.#matricula = value;
 	}
 
-	todosUsuarios()
+	get curso()
 	{
-		return this.usuarios;
+		return this.#curso;
 	}
 
-	numeroDeUsuarios()
+	set curso(value)
 	{
-		return this.usuarios.length;
+		this.#curso = value;
 	}
 
-	getUsuario(nome)
+	get periodo()
 	{
-		return this.usuarios.find((user) => user.name == nome);
+		return this.#periodo;
 	}
 
-	checkPassword(nome, senha)
+	set periodo(value)
 	{
-		return this.getUsuario(nome).password == senha;
+		this.#periodo = value;
 	}
 
-	isCadastrado(objUsuario)
+	get disciplinas()
 	{
-		return this.usuarios.includes(objUsuario);
+		return this.#disciplinas;
 	}
 
-	isCadastradoNome(nome)
+	set adicionarDisciplinas(value)
 	{
-		return this.usuarios.find((user) => user.name == nome) != undefined;
+		this.#disciplinas.push(value);
+	}
+}
+
+class Teacher extends User
+{
+	#departamento
+	#disciplinas = [];
+
+	constructor(username, password, name)
+	{
+		super(username, password, "teacher", name);
 	}
 
-	isCadastradoEmail(email)
+
+	get departamento()
 	{
-		return this.usuarios.find((user) => user.email == email) != undefined;
+		return this.#departamento;
 	}
+
+	set departamento(value)
+	{
+		this.#departamento = value;
+	}
+
+	get disciplinas()
+	{
+		return this.#disciplinas;
+	}
+
+	set adicionarDisciplinas(value)
+	{
+		this.#disciplinas.push(value);
+	}
+
+
 }
 
 class Disciplinas
 {
-	constructor(nome, tipo, detalhe)
+	#id;
+	#name;
+	#workload;
+	#teacherId;
+	#courseId;
+
+	constructor(name, codigo, workload, teacher, course)
 	{
-		this.name = nome;
-		this.tipo = tipo;
-		this.detalhe = detalhe;
+		this.#name = name;
+		this.#id = codigo;
+		this.#workload = workload;
+		this.#teacherId = teacher;
+		this.#courseId = course;
 	}
+
+	get id()
+	{
+		return this.#id;
+	}
+
+	set id(value)
+	{
+		this.#id = value;
+	}
+
+	get name()
+	{
+		return this.#name;
+	}
+
+	set name(value)
+	{
+		this.#name = value;
+	}
+
+	get workload()
+	{
+		return this.#workload;
+	}
+
+	set workload(value)
+	{
+		this.#workload = value;
+	}
+
+	get teacherId()
+	{
+		return this.#teacherId;
+	}
+
+	set teacherId(value)
+	{
+		this.#teacherId = value;
+	}
+
+	get courseId()
+	{
+		return this.#courseId;
+	}
+
+	set courseId(value)
+	{
+		this.#courseId = value;
+	}
+
 }
 
 class TableCreator
 {
 	static currentPage = 1;
-	static filteredData;
 
 	static create(data)
 	{
@@ -354,18 +576,11 @@ class TableCreator
 	static handleSearch()
 	{
 		const query = this.searchBox.value.toLowerCase();
-		this.filteredData = this.data.filter(item =>
-			Object.values(item).some(value =>
-				typeof value === "string" && value.toLowerCase().includes(query)
-			)
-		);
+		this.filteredData = this.data.filter(item => Object.values(item).some(value => typeof value === "string" && value.toLowerCase().includes(query)));
 		this.currentPage = 1;
 		this.updateTable();
 	}
 }
-
-let usuariosCadastrados = new UserPool();
-console.log(usuariosCadastrados.todosUsuarios());
 
 function registerUser()
 {
@@ -428,8 +643,7 @@ function logout()
 	if (isLoggedIn())
 	{
 		sessionStorage.removeItem("loginSession");
-	}
-	else
+	} else
 	{
 		alert("You are not logged into any account!");
 	}
@@ -449,7 +663,6 @@ function canLoadProtectedPage()
 	if (isLoggedIn() && location.href.split("/").slice(-1).join("") == "index.html")
 	{
 		window.location = "./loggedOverview.html";
-		return;
 	}
 }
 
@@ -468,20 +681,14 @@ function createNavLink(name, href, parentUl)
 
 function showLoggedNavBar()
 {
-	let navBarLinks = [
-		["Home", "./index.html"],
-		["Disciplinas", "./disciplinas.html"],
-		["Cadastro", "./cadastro.html"],
-		["Alunos", "./alunos.html"],
-	];
+	let navBarLinks = [["Home", "./index.html"], ["Disciplinas", "./disciplinas.html"], ["Cadastro", "./cadastro.html"], ["Alunos", "./alunos.html"],];
 
 	if (!isLoggedIn())
 	{
 		let navBarUl = document.querySelector(".nav-ul");
 		navBarUl.replaceChildren();
 		createNavLink(navBarLinks[0][0], navBarLinks[0][1], navBarUl);
-	}
-	else
+	} else
 	{
 		let navBarUl = document.querySelector(".nav-ul");
 		navBarUl.replaceChildren();
